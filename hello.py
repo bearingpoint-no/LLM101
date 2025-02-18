30f889434a71eb0859942eed4a973ebea6eb170d
KasparaGasvar-BP <kaspara.gasvar@bearingpoint.com>
2025-02-18 20:08:35 +0100

    Satt opp miljø

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from llm101!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ llm.py
@@ -0,0 +1,20 @@
+from langchain_openai import AzureChatOpenAI
+from dotenv import find_dotenv, load_dotenv
+import os
+#from utils.timer import timer
+
+# Get environment variables
+load_dotenv(find_dotenv(), override=True)
+
+class Llm:
+    # Connect to llm
+    llm = AzureChatOpenAI(
+        azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_GPT_4O_MINI"],
+        model=os.environ.get("OPENAI_MODEL_GPT_4O-MINI", default="gpt-4o-mini"),
+        temperature=0,
+    )
+
+LLM = Llm()
+
+
+print(Llm.llm.invoke("whats up?"))
