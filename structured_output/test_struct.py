9eae27fadcda87c9a9783d3d8654ba6cce6f2e90
KasparaGasvar-BP <kaspara.gasvar@bearingpoint.com>
2025-03-19 19:36:28 +0100

    struktur eller?

--- structured_output/test_struct.py
+++ structured_output/test_struct.py
@@ -1,8 +1,8 @@
 from utils.llm import LLM
-from utils.categorize_1 import Categorize, Categorize_bound
-from utils.dataset_c import News_dataset
+from utils.categorize_1 import Categorize_bound, Categorize_think
+from utils.dataset_c import FeedbackData
 
-
+"""
 def open_categories() -> None:
     structured_llm = LLM.llm.with_structured_output(Categorize)
     for i in range(0, 10):
@@ -15,12 +15,12 @@ def open_categories() -> None:
 
 def bound_categories() -> None:
     structured_llm = LLM.llm.with_structured_output(Categorize_bound)
-    task = """Categorize the folowing news article into the best fitting category. Allowed categories:\n
+    task = "Categorize the folowing news article into the best fitting category. Allowed categories:\n
             1 - World\n
             2 - Sports\n
             3 - Business\n
                 4 - Sci/Tech\n\n
-                """
+                "
     for i in range(0, 10):
         response = structured_llm.invoke(
             task + f"Article:\n'{News_dataset.descriptions[i]}'"
@@ -39,3 +39,17 @@ def loop_structured(task: str, structure_type, num_reps: int) -> None:
 loop_structured(
     task="Categorise this news article: ", structure_type=Categorize, num_reps=10
 )
+""" 
+
+
+
+def loop_structured(task: str, structure_type, num_reps: int) -> None:
+    structured_llm = LLM.llm.with_structured_output(structure_type)
+
+    for i in range(num_reps):
+        response = structured_llm.invoke(task + f"\n'{FeedbackData.df["Feedback"][i]}'")
+        print(response)
+
+
+t = 'Categorize the following feedback from an IT-questionnaire:'
+loop_structured(t,Categorize_think, 10)
