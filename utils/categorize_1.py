188d81683e8f370f9dab03d123a092360bc2ef4a
KasparaGasvar-BP <kaspara.gasvar@bearingpoint.com>
2025-03-19 17:14:14 +0100

    noob

--- utils/categorize_1.py
+++ utils/categorize_1.py
@@ -4,8 +4,8 @@ from typing import Optional
 
 class Categorize(BaseModel):
     """Categorization of news article."""
-
-    cat_1: str = Field(description="The best fitting geenral category")
+    #Thoughts: Forklar tankegangen din.
+    cat_1: str = Field(description="The best fitting genral category")
     cat_2: str = Field(description="The second to best fitting general category")
     rating: Optional[int] = Field(
         description="Your certainty of the corectness of the best category on a scale from 1-10"
@@ -19,5 +19,6 @@ class Categorize_bound(BaseModel):
         description="The number corresponding to the best fitting category."
     )
     certainty: float = Field(
-        description="Ceratinty of correctness in categorizing (0-1 scale)."
+        description="Certainty of correctness in categorizing (0-1 scale)."
     )
+
